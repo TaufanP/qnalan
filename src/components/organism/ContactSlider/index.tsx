250d65fb97f9d9b32d7899a1f599506547ad4e0a
Taufanp <tefanzprih@gmail.com>
2021-06-19 10:06:11 +0700

    Prettify ContactSlider

--- src/components/organism/ContactSlider/index.tsx
+++ src/components/organism/ContactSlider/index.tsx
@@ -1,81 +1,27 @@
 import React from "react";
 import { View, Text } from "react-native";
-
-const THUMB_RADIUS = 12;
+import styles from "./styles";
 
 const Notch = (props: any) => {
-  return (
-    <View
-      style={{
-        width: 8,
-        height: 8,
-        borderLeftColor: "transparent",
-        borderRightColor: "transparent",
-        borderTopColor: "#4499ff",
-        borderLeftWidth: 4,
-        borderRightWidth: 4,
-        borderTopWidth: 8,
-      }}
-      {...props}
-    />
-  );
+  return <View style={styles.notchStyle} {...props} />;
 };
+
 const Thumb = () => {
-  return (
-    <View
-      style={{
-        width: THUMB_RADIUS * 2,
-        height: THUMB_RADIUS * 2,
-        borderRadius: THUMB_RADIUS,
-        borderWidth: 2,
-        borderColor: "#7f7f7f",
-        backgroundColor: "#ffffff",
-      }}
-    />
-  );
+  return <View style={styles.thumbStyle} />;
 };
+
 const Rail = () => {
-  return (
-    <View
-      style={{
-        width: "100%",
-        height: 4,
-        borderRadius: 2,
-        backgroundColor: "#7f7f7f",
-      }}
-    />
-  );
+  return <View style={styles.railStyle} />;
 };
+
 const RailSelected = () => {
-  return (
-    <View
-      style={{
-        height: 4,
-        backgroundColor: "#4499ff",
-        borderRadius: 2,
-      }}
-    />
-  );
+  return <View style={styles.railSelectedStyle} />;
 };
+
 const Label = ({ text, ...restProps }: any) => {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        padding: 8,
-        backgroundColor: "#4499ff",
-        borderRadius: 4,
-      }}
-      {...restProps}
-    >
-      <Text
-        style={{
-          fontSize: 16,
-          color: "#fff",
-        }}
-      >
-        {text}
-      </Text>
+    <View style={styles.labelStyle} {...restProps}>
+      <Text style={styles.labelTextStyle}>{text}</Text>
     </View>
   );
 };
