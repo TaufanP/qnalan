23f212049939529cd6065c0a741c2ae04feefadd
Taufanp <tefanzprih@gmail.com>
2021-06-26 16:00:02 +0700

    On tap notif

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import messaging from "@react-native-firebase/messaging";
+import { Linking } from "react-native";
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  //   console.log(remoteMessage);
+});
+
+messaging().onNotificationOpenedApp((message) =>
+  Linking.openURL(message.data.linking)
+);
 
 AppRegistry.registerComponent(appName, () => App);
